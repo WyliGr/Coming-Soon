c96a890073544e1de68bde292f60b7e75b3464b5
Wyliam Burger <wyliam.burger@proton.me>
2026-02-02 04:59:44 +0100

    fix: allowedHosts

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true, // Autorise l’accès depuis d’autres appareils
+    allowedHosts: ['wyliam.fr', '*.wyliam.fr', 'localhost'],
+  },
   vite: {
     plugins: [tailwindcss()]
   }
